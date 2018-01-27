fd89a57d7ae9ff7be2d4898e9ed2e8bc29af5827
Tommi Ripatti <ripatti.tommi@gmail.com>
2018-01-27 06:23:00 +0200

    + Modified ehco

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ var port = 4000;
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   var url_parts = url.parse(request.url);
-  response.end(url_parts.pathname + '\n');
+  response.end('Echo: ' + url_parts.pathname + '\n');
 });
 
 server.listen(port)
